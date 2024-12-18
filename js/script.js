58fd67208f15544f8e67d4556b9b90e7490a19a3
Michele Lescano <104702362+MicheleLescano@users.noreply.github.com>
2024-12-17 21:59:37 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const tempo = document.getElementById("tempo")
+const fim = document.getElementById("fim")
+
+
+function venceu() {
+    if(controle) {
+        clearInterval(timer)
+        fim.innerHTML = " Champion!!!🏆"
+        fim.style.display = "block"
+    }
+}
+
+function bateu() {
+    if(controle) {
+    clearInterval( timer) 
+    fim.style.display = "block"
+    }
+}
+
+var min = 1
+var seg = 60
+var timer
+var controle = false 
+
+function iniciar() {
+    controle = true
+
+     timer = setInterval( function() {
+        seg--
+
+        if( seg == -1 ) {
+            min--
+            seg = 59
+    }
+
+    tempo.innerHTML =  (seg < 10) ? min + ":0" + seg : min + ":" + seg 
+
+
+    if( min == 0 && seg == 0 ) {
+        clearInterval( timer) 
+        fim.style.display = "block"
+    }
+
+    if ( min == 0 && seg <= 10 ) {
+        tempo.style.color = "orange"
+    }
+    }, 1000)
+}
